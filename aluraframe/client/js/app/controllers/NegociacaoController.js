f2b87f2bc4530a9bbed6ddab7d2066b8d1e2f6eb
JPedro <jpedro0brito@gmail.com>
2020-01-30 10:32:48 -0400

    acidionando uma lista de negocio inmutavel para que nao seja alterando

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,19 +5,31 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputData = $('#data');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
 
-        let negociacao = new Negociacao(
-            new DateHelper()
-                .textoParaData(this._inputData.value),
+        this._listaNegociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criarNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
 
-        console.log(negociacao);
+        this._inputData.focus();
     }
 
 }
--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,17 @@
 class DateHelper{
-    textoParaData(texto){
+    constructor(){
+        throw new Error('Esta classe não pode sert instanciada');
+    }
+
+    static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+            
         return new Date(...texto
             .split('-')
             .map((item, index) => item - index % 2));
     }
-    dataParaTexto(data){
-        return data.getDate()
-        +'/'+ (data.getMonth() + 1)
-        +'/'+ data.getFullYear();
+    static dataParaTexto(data){
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 }
