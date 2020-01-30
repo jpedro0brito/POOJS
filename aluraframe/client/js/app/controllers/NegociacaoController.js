79c33d420fc4f4d108c262080cf793bb7f662473
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-01-29 22:11:34 -0300

    Acidionando controller de Negociacao no projeto e aplicando a action de adicionar

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,19 @@
+class NegociacaoController{
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this.inputQuantidade = $('#quantidade');
+        this.inputData = $('#data');
+        this.inputValor = $('#valor');
+    }
+
+    adiciona(event){
+        event.preventDefault();
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
+
+    }
+
+}
