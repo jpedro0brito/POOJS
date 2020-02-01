ea62151673482d1c1ce557339acf7a1cc25953ec
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-02-01 18:38:34 -0300

    encapsulado minha classe connection  para que os usuario nao techa acesso aos atributos da classe para isso utilizaremos o padrao de projeto molude pattern

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,38 +1,43 @@
-const stores = ['negociacoes'];
-const version = 1;
-const dbName = 'aluraframe';
+var ConnectionFactory = (function (){
+    const stores = ['negociacoes'];
+    const version = 1;
+    const dbName = 'aluraframe';
 
-class ConnectionFactiory{
-    constructor(){
-        throw new Error('Não é possivel criar instancias de connnerctionFactory');
+    let connection = null;
+
+    return class ConnectionFactiory{
+        constructor(){
+            throw new Error('Não é possivel criar instancias de connnerctionFactory');
+        }
+
+        static getConnection(){
+            return new Promise((resolve, reject)=>{
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e =>{
+                    ConnectionFactory_createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e =>{
+                    if(!connection) connection = e.target.result;
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e =>{
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                }
+            });
+        }
+
+        static _createStores(connection){
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)){
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, {autoIncrement: true});
+            })
+        }
     }
-
-    static getConnection(){
-        return new Promise((resolve, reject)=>{
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e =>{
-                ConnectionFactory_createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e =>{
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e =>{
-                console.log(e.target.error);
-                reject(e.target.error.name);
-            }
-        });
-    }
-
-    static _createStores(connection){
-        stores.forEach(store => {
-            if(connection.objectStoreNames.contains(store)){
-                connection.deleteObjectStore(store);
-            }
-
-            connection.createObjectStore(store, {autoIncrement: true});
-        })
-    }
-}
+})();
