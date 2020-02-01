2fc41589715497d838c6cce43fd35f701c01ff32
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-02-01 18:26:54 -0300

    quadando os dados da negociacao no indexeddb para isso criaremos um classe connection usando o patao de projeto Factiory, promise

---
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,38 @@
+const stores = ['negociacoes'];
+const version = 1;
+const dbName = 'aluraframe';
+
+class ConnectionFactiory{
+    constructor(){
+        throw new Error('Não é possivel criar instancias de connnerctionFactory');
+    }
+
+    static getConnection(){
+        return new Promise((resolve, reject)=>{
+            let openRequest = window.indexedDB.open(dbName, version);
+
+            openRequest.onupgradeneeded = e =>{
+                ConnectionFactory_createStores(e.target.result);
+            };
+
+            openRequest.onsuccess = e =>{
+                resolve(e.target.result);
+            };
+
+            openRequest.onerror = e =>{
+                console.log(e.target.error);
+                reject(e.target.error.name);
+            }
+        });
+    }
+
+    static _createStores(connection){
+        stores.forEach(store => {
+            if(connection.objectStoreNames.contains(store)){
+                connection.deleteObjectStore(store);
+            }
+
+            connection.createObjectStore(store, {autoIncrement: true});
+        })
+    }
+}
