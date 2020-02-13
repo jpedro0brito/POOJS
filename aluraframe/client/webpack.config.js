2750921d7bc498ec7e895aa890b88b528e25828f
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-02-12 23:30:32 -0400

    Otimizando o build com Scope Hoisting para se mais rapido

--- aluraframe/client/webpack.config.js
+++ aluraframe/client/webpack.config.js
@@ -18,6 +18,7 @@ plugins.push(
 );
 
 if(process.env.NODE_ENV == 'production'){
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
     plugins.push(
         new optimizeCSSAssetsPlugin({
