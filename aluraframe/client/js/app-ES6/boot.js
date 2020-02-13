0a866e919aeaa5f22004ec64e1558a7f99312ea4
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-02-12 23:27:16 -0400

    adicionando o jquery no projeto

--- aluraframe/client/js/app-ES6/boot.js
+++ aluraframe/client/js/app-ES6/boot.js
@@ -2,6 +2,7 @@ import {currentInstance} from './controllers/NegociacaoController';
 import {} from './polyfill/fetch';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/js/modal.js';
 
 let negociacaoController = currentInstance();
 
